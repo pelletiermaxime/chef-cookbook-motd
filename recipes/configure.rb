5e67829940dd703363e97f2aff1aeafabbe855f5
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-05-25 01:42:37 -0400

    No need to do the funky linking of var/run/motd in /etc/motd when not in debian

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -4,11 +4,12 @@ directory '/etc/update-motd.d/' do
   action :create
 end
 
-file "/etc/motd" do
-  action :delete
-  #only_if "! test -L /etc/motd" #Delete only if not a symlink
-end
+if node["platform"] == "debian"
+    file "/etc/motd" do
+      action :delete
+    end
 
-link "/etc/motd" do
-  to "/var/run/motd"
+    link "/etc/motd" do
+      to "/var/run/motd"
+    end
 end
