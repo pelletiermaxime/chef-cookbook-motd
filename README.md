b3ce4181136fa986b8ffbe3cd3b774c6aa4114a5
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-05-25 01:47:27 -0400

    Creds where it is due

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Usage
 ============
 
 
+Thanks to
+=========
+
+* This tutorial showing the most simple way to do custom motd on debian:
+  http://oitibs.com/debian-wheezy-dynamic-motd/
--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'motd'
 maintainer       'Maxime Pelletier'
 maintainer_email 'pelletiermaxime@gmail.com'
 description      'Cookbook to install custom motds'
-#long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 #depends          ''
